a4b72ac38edcc88645b12bf08839f88fe9d9d41a
aktives <921355585@qq.com>
2025-01-20 01:05:41 +0800

    add curriculum

---
+++ src/component/mainTab/curriculum.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+interface ColumnContentProps {
+    content: string;
+}
+
+export const LeftColumnContent = () => (
+    <View>
+    </View>
+);
+
+export const RightColumnContent = () => (
+    <View>
+    </View>
+);
--- src/screen/mainTabScreen/CurriculumScreen.tsx
+++ src/screen/mainTabScreen/CurriculumScreen.tsx
@@ -1,13 +1,47 @@
 import React from 'react';
 import {
     View,
-    Text,
+    StyleSheet,
+    ImageBackground
 } from 'react-native';
+import { LeftColumnContent, RightColumnContent } from '../../component/mainTab/curriculum';
+
+const styles = StyleSheet.create({
+    background: {
+      flex: 1,
+      flexDirection: 'row',
+      justifyContent: 'center',
+    },
+    column: {
+      height: '100%',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    leftColumn: {
+      backgroundColor: 'rgba(184, 107, 107, 0.7)', 
+      flex: 0.2,
+    },
+    rightColumn: {
+      backgroundColor: 'rgba(164, 139, 139, 0.27)', 
+      flex: 1.4,
+    },
+  });
 
 export default function ScheduleScreen(): React.JSX.Element {
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Schedule Screen</Text>
-        </View>
+        <>
+           <ImageBackground 
+              source={require('../../assets/qwq.png')}
+               style={styles.background}
+               >
+               <View style={[styles.column, styles.leftColumn]}>
+                    <LeftColumnContent/>
+                </View>
+
+                <View style={[styles.column, styles.rightColumn]}>
+                    <RightColumnContent/>
+                </View>
+            </ImageBackground>
+       </>
     );
 }
