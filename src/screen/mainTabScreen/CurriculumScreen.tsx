b073bf0a1330986318dc5a439bf63a4a774ed8d2
aktives <921355585@qq.com>
2025-01-20 16:46:31 +0800

    little change

--- src/screen/mainTabScreen/CurriculumScreen.tsx
+++ src/screen/mainTabScreen/CurriculumScreen.tsx
@@ -1,30 +1,43 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { ImageBackground, ScrollView, View, Dimensions } from 'react-native';
 import Curriculum from '../../component/mainTab/curriculum';
 
 const { width } = Dimensions.get('window');
 
-export default function ScheduleScreen(): React.JSX.Element {
-    const curriculumItems = [1, 2, 3].map((item) => (
-        <View key={item} style={{ width, height: '100%' }}>
-            <Curriculum />
-        </View>
-    ));
+interface ScheduleScreenProps {
+  initialPage?: number; 
+}
 
-    return (
-        <ImageBackground 
-            source={require('../../assets/qwq.png')}
-            style={{ flex: 1 }}
-        >
-            <ScrollView 
-                horizontal={true}
-                snapToInterval={width} 
-                decelerationRate="fast" 
-                showsHorizontalScrollIndicator={false}
-                contentContainerStyle={{ flexGrow: 1 }} 
-            >
-                {curriculumItems}
-            </ScrollView>
-        </ImageBackground>
-    );
+export default function ScheduleScreen({ initialPage = 0 }: ScheduleScreenProps): React.JSX.Element {
+  const scrollViewRef = useRef<ScrollView>(null);
+
+  useEffect(() => {
+    if (scrollViewRef.current && initialPage >= 0) {
+      scrollViewRef.current.scrollTo({ x: initialPage * width, animated: true });
+    }
+  }, [initialPage, width]);
+
+  const curriculumItems = [1, 2, 3].map((item, index) => (
+    <View key={index} style={{ width, height: '100%' }}>
+      <Curriculum />
+    </View>
+  ));
+
+  return (
+    <ImageBackground 
+        source={require('../../assets/qwq.png')}
+        style={{ flex: 1 }}
+    >
+      <ScrollView 
+          ref={scrollViewRef}
+          horizontal={true}
+          snapToInterval={width}
+          decelerationRate="fast"
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={{ flexGrow: 1 }}
+      >
+        {curriculumItems}
+      </ScrollView>
+    </ImageBackground>
+  );
 }
